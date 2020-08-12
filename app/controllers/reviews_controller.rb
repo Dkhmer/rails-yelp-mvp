464891a14fb12c3a988434716b9809b1b8806c61
David Kuoch <kuoch0305@gmail.com>
2020-08-12 17:00:03 +0200

    rake ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # def restaurant_params
+  #   params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  # end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Chez TOTO', address: "18 villa ddadad", category: 'chinese')
